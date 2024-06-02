809f64288ada998d97ac332004215c4bca7f21c8
JonathasSC <jonathas.s.cardoso28@gmaill.com>
2024-06-02 15:56:32 -0300

    feat: added Dockerfile

--- backend/api/src/server.js
+++ backend/api/src/server.js
@@ -1,6 +1,7 @@
 const usersRouter = require("./routes/usersRoutes.js");
 const commentsRouter = require("./routes/commentsRoutes.js");
 const postsRouter = require("./routes/postsRoutes.js");
+const authRouter = require("./routes/authRoutes.js");
 
 const express = require("express");
 const cors = require("cors");
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(cors({ origin: true }));
 
+app.use("/auth", authRouter);
 app.use("/users", usersRouter);
 app.use("/posts", postsRouter);
 app.use("/comments", commentsRouter);
